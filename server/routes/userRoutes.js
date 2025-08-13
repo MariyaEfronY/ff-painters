7d6f3448bc729590b6fa00b8501226d0c1868176
MariyaEfronY <mariyaefrony@gmail.com>
2025-08-13 10:00:47 +0530

    u edit page

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,27 @@
 import express from "express";
-import { registerUser, loginUser, getUserProfile, updateUserProfile, getUserDashboard } from "../controllers/userController.js";
+import {
+  registerUser,
+  loginUser,
+  getUserProfile,
+  updateUserProfile,
+  getUserDashboard
+} from "../controllers/userController.js";
 import userProtect from "../middleware/userAuthMiddleware.js";
-
-
+import multer from "multer";
+import path from "path";
 
 const router = express.Router();
+
+// Multer storage config
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/userprofileImages");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
+const upload = multer({ storage });
 
 // Public routes
 router.post("/signup", registerUser);
@@ -13,7 +30,6 @@ router.post("/login", loginUser);
 // Protected routes
 router.get("/profile", userProtect, getUserProfile);
 router.get("/dashboard", userProtect, getUserDashboard);
-
-router.put("/profile", protect, upload.single("profileImage"), updateUserProfile);
+router.put("/profile", userProtect, upload.single("profileImage"), updateUserProfile);
 
 export default router;
